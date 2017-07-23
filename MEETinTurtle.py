a545bee4119fadcd2231b08efb488d86ba375f08
marah19-meet <marah19@meet.mit.edu>
2017-07-23 19:00:30 +0300

    Stopping here

---
+++ MEETinTurtle.py
@@ -0,0 +1,15 @@
+import turtle
+
+turtle.penup()
+turtle.goto(-200,-100)
+turtle.pendown()
+turtle.goto(-200,-100+200)
+turtle.goto(-200+50,-100)
+turtle.goto(-200+100,-100+200)
+turtle.goto(-200+100,-100)
+
+turtle.penup()
+turtle.goto(-100+150,-100)
+turtle.pendown()
+turtle.goto
+turtle.mainloop()
